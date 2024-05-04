07ce04ac08ea5ad67b9238b0252d8bc5117f5fde
CoolBvll <80421454+CoolBvll@users.noreply.github.com>
2024-05-04 19:49:17 +0300

    archerToggle.js

--- archerToggle.js
+++ archerToggle.js
@@ -4,13 +4,21 @@ if (!World.isWorldLoaded()) JsMacros.waitForEvent('ChunkLoad')
 var ARCHER_TOGGLE_CONFIG = require('./archerToggle.json');
 let archerToggle = ARCHER_TOGGLE_CONFIG.archerToggle;
 const CommandManager = Chat.getCommandManager();
-let command = CommandManager.createCommandBuilder("archer");
+let command = CommandManager.createCommandBuilder("meleeMacro");
 command.unregister();
 
-command.greedyStringArg("true/false").executes(JavaWrapper.methodToJava((event) => {
-    
+command.wordArg("medule").suggestMatching(["archer"]).wordArg("true/false").suggestMatching(["true", "false"]).executes(JavaWrapper.methodToJava((event) => {
+
         archerToggle = event.getArg("true/false");
-        Chat.log("Archer toggle: " + archerToggle);
+
+        if (archerToggle == "true"){
+                Chat.log("You are now on archer mode.")
+        }
+        else{
+                Chat.log("You are no longer on archer mode.");
+        }
+        
+
         FS.open('./archerToggle.json').write(JSON.stringify({ archerToggle: archerToggle }));
     
 })).register();
